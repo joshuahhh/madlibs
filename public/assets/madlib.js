2adc096546cb3722f2c77cf96dbf537f098879c8
paulate <paula.m.te@gmail.com>
2016-05-15 09:42:00 -0700

    cleaning + commenting
    and some interface edits

--- public/assets/madlib.js
+++ public/assets/madlib.js
@@ -1,6 +1,6 @@
 function formatMadlib(a) {
     for (; a.match(blankRe); )
-        a = a.replace(blankRe, '<span class="blank"><input type="text" class="blank-input" autocomplete="off" /><span class="blank-pos">$1</span></span>');
+        a = a.replace(blankRe, '<span class="blank"><input type="text" class="blank-input" autocomplete="off"/><span class="blank-pos">$1</span></span>');
     return a
 }
 var blankRe = /{([^{}]+)}/;
